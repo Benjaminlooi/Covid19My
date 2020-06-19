6de8aeb32efb51f9ef1ea6dd7bfda559ec9bfb2d
Benjaminlooi <benjaminlooidt@gmail.com>
2020-06-19 17:26:49 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,12 @@ module.exports = {
   transpileDependencies: ["vuetify"],
   devServer: {
     host: 'localhost'
+  },
+  pwa: {
+    name: 'Covid19 Malaysia',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
   }
 };
