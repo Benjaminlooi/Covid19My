ab574b76bc910f3cfffa05ea4eb03c7cdcc45eab
Benjaminlooi <benjaminlooidt@gmail.com>
2020-06-19 12:15:04 +0800

    .

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -13,6 +13,43 @@
       </v-row>
     </v-container>
     <v-container id="main-component">
+      <v-row>
+        <v-col>
+          <v-card class="mx-auto" color="#26c6da" dark max-width="400">
+            <v-card-title>
+              <v-icon large left>mdi-twitter</v-icon>
+              <span class="title font-weight-light">Twitter</span>
+            </v-card-title>
+
+            <v-card-text
+              class="headline font-weight-bold"
+            >"Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."</v-card-text>
+
+            <v-card-actions>
+              <v-list-item class="grow">
+                <v-list-item-avatar color="grey darken-3">
+                  <v-img
+                    class="elevation-6"
+                    src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+                  ></v-img>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                  <v-list-item-title>Evanve</v-list-item-title>
+                </v-list-item-content>
+
+                <v-row align="center" justify="end">
+                  <v-icon class="mr-1">mdi-heart</v-icon>
+                  <span class="subheading mr-2">256</span>
+                  <span class="mr-1">·</span>
+                  <v-icon class="mr-1">mdi-share-variant</v-icon>
+                  <span class="subheading">45</span>
+                </v-row>
+              </v-list-item>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
       <!-- <v-row>
       <v-col class="mb-5" cols="12">
         <TheSparkline />
@@ -75,7 +112,7 @@ export default {
   }
 }
 
-#main-component{
+#main-component {
   max-width: 400px;
 }
 </style>
