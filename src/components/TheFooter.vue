2e8f121430f458f53878483f806de305d6bce6a8
Benjamin Looi <benjaminlooidt@gmail.com>
2020-06-23 16:57:13 +0800

    Update TheFooter.vue

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <v-footer>
     <v-spacer></v-spacer>
-    <div>&copy; {{ new Date().getFullYear() }} Bensanity v4</div>
+    <div>&copy; {{ new Date().getFullYear() }} Bensanity v5</div>
   </v-footer>
 </template>
