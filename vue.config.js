7a6958d09c77dbbf88a69821d66618ac5621d18b
Benjamin Looi <benjaminlooidt@gmail.com>
2020-06-23 16:03:39 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,12 @@ module.exports = {
     name: 'Covid19 Malaysia',
     themeColor: '#4DBA87',
     msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
+    appleMobileWebAppCapable: 'no',
+    appleMobileWebAppStatusBarStyle: 'default',
+    assetsVersion: '1.0',
+    manifestOptions: {
+      start_url: "https://covid19my.netlify.app/",
+      display: "standalone"
+    }
   }
 };
