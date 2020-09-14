4da42854c57d173efaedc1de564b1cc175d71c1e
Benjamin Looi <benjaminlooidt@gmail.com>
2020-09-14 11:21:52 +0800

    Add logo images

--- vue.config.js
+++ vue.config.js
@@ -14,6 +14,13 @@ module.exports = {
       start_url: "https://covid19my.netlify.app/",
       display: "standalone"
     },
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    },
     workboxOptions: {
       exclude: ['_redirects', 'assetlinks.json'],
     }
