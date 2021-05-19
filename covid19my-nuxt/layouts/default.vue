a6e983552d5dfa058914cb5a85ad3f6a153c0e49
Benjamin Looi <benjaminlooidt@gmail.com>
2021-05-19 19:28:52 +0800

    Fix font import not working in Nuxt (#8)
    * Fix font import not working in Nuxt
    
    * Fix font import not working in Nuxt

--- covid19my-nuxt/layouts/default.vue
+++ covid19my-nuxt/layouts/default.vue
@@ -50,8 +50,6 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
-
 .purple-gradient-bg {
   background: rgb(108, 60, 228) !important;
   background: linear-gradient(
